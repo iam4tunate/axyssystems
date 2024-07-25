362668460167723e0ed35566e216f41a41a1a51e
iam4tunate <ogodujoseph@gmail.com>
2024-07-25 06:16:06 +0100

    adjusted p tag font size

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -4,7 +4,7 @@ export function Activity({ heading, paragraph, image }) {
       <h2 className='text-primary text-2xl font-clashSemibold pb-2 leading-6'>
         {heading}
       </h2>
-      <p className='leading-[20px]'>{paragraph}</p>
+      <p className='leading-[22px]'>{paragraph}</p>
       <img src={image} alt='' className='mt-6 justify-self-end self-end' />
     </div>
   );
